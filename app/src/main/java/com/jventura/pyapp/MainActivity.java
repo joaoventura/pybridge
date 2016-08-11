6ed12f86e023076246cd785a9e6d4b0e8c6d13a4
João Ventura <joaojonesventura@gmail.com>
2016-08-11 12:43:49 +0100

    Implement usage example

--- app/src/main/java/com/jventura/pyapp/MainActivity.java
+++ app/src/main/java/com/jventura/pyapp/MainActivity.java
@@ -2,6 +2,14 @@ package com.jventura.pyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.jventura.pybridge.AssetExtractor;
+import com.jventura.pybridge.PyBridge;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Extract python files from assets
+        AssetExtractor assetExtractor = new AssetExtractor(this);
+        assetExtractor.removeAssets("python");
+        assetExtractor.copyAssets("python");
+
+        // Get the extracted assets directory
+        String pythonPath = assetExtractor.getAssetsDataDir() + "python";
+
+        // Start the Python interpreter
+        PyBridge.start(pythonPath);
+
+        // Call a Python function
+        try {
+            JSONObject json = new JSONObject();
+            json.put("function", "greet");
+            json.put("name", "Python 3.5");
+
+            JSONObject result = PyBridge.call(json);
+            String answer = result.getString("result");
+
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText(answer);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Stop the interpreter
+        PyBridge.stop();
     }
 }
