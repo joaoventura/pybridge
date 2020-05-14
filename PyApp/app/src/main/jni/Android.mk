1db5b72f66f2312facbf3b5550e967aa0694f296
João Ventura <joaojonesventura@gmail.com>
2020-05-14 18:05:36 +0100

    Compile pybridge.so using python3.8

--- PyApp/app/src/main/jni/Android.mk
+++ PyApp/app/src/main/jni/Android.mk
@@ -1,5 +1,10 @@
 LOCAL_PATH := $(call my-dir)
-CRYSTAX_PATH := /Users/jventura/Library/Android/crystax-ndk-10.3.2
+
+# Python-for-Android paths
+
+PYTHON_FOR_ANDROID_PATH := /Users/jventura/Desktop/Shared/python-for-android
+GUEST_PYTHON_PATH := $(PYTHON_FOR_ANDROID_PATH)/build/other_builds/python3
+PYTHON_PATH := $(GUEST_PYTHON_PATH)/armeabi-v7a__ndk_target_21/python3
 
 
 # Build libpybridge.so
@@ -8,14 +13,19 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := pybridge
 LOCAL_SRC_FILES := pybridge.c
 LOCAL_LDLIBS := -llog
-LOCAL_SHARED_LIBRARIES := python3.5m
+LOCAL_SHARED_LIBRARIES := python3.8m
 include $(BUILD_SHARED_LIBRARY)
 
 
-# Include libpython3.5m.so
+# Include libpython3.8m.so
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := python3.5m
-LOCAL_SRC_FILES := $(CRYSTAX_PATH)/sources/python/3.5/libs/$(TARGET_ARCH_ABI)/libpython3.5m.so
-LOCAL_EXPORT_CFLAGS := -I $(CRYSTAX_PATH)/sources/python/3.5/include/python/
+LOCAL_MODULE    := python3.8m
+LOCAL_SRC_FILES := $(PYTHON_PATH)/android-build/libpython3.8m.so
+LOCAL_EXPORT_CFLAGS := -I $(PYTHON_PATH)/Include
 include $(PREBUILT_SHARED_LIBRARY)
+
+
+# The assets path
+# ASSETS_PATH := $(PYTHON_FOR_ANDROID_PATH)/dists/unnamed_dist_1__armeabi-v7a/_python_bundle/_python_bundle
+# NOTE: Copy assets from the p4a dist path to ../assets/python along with bootstrap.py
