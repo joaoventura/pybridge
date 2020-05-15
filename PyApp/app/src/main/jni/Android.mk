b60db5dafda01d19c98b1be8d7671a15c97f3c6a
João Ventura <joaojonesventura@gmail.com>
2020-05-15 09:15:21 +0100

    Include p4a compiled python3.8 in project

--- PyApp/app/src/main/jni/Android.mk
+++ PyApp/app/src/main/jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 # Python-for-Android paths
 
-PYTHON_FOR_ANDROID_PATH := /Users/jventura/Desktop/Shared/python-for-android
+PYTHON_FOR_ANDROID_PATH := $(LOCAL_PATH)/../../../../../python-for-android
 GUEST_PYTHON_PATH := $(PYTHON_FOR_ANDROID_PATH)/build/other_builds/python3
 PYTHON_PATH := $(GUEST_PYTHON_PATH)/armeabi-v7a__ndk_target_21/python3
 
